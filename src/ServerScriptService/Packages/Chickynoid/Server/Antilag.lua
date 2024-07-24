795c5be80737326a8f892ec583e4f51bd481ccc5
MrJ <155217642+golden314@users.noreply.github.com>
2024-07-23 19:41:00 -0700

    Add animations

--- src/ServerScriptService/Packages/Chickynoid/Server/Antilag.lua
+++ src/ServerScriptService/Packages/Chickynoid/Server/Antilag.lua
@@ -4,10 +4,42 @@ module.history = {}
 module.temporaryPositions = {}
 
 local path = game.ReplicatedFirst.Packages.Chickynoid
+local Keyframes = require(path.Shared.Simulation.Keyframes).new()
+local Animations = require(path.Shared.Simulation.Animations)
 local Enums = require(path.Shared.Enums)
+
+local animStates = {
+	{
+		"animCounter0",
+		"animNum0",
+	}
+	{
+		"animCounter1",
+		"animNum1",
+	}
+	{
+		"animCounter2",
+		"animNum2",
+	}
+	{
+		"animCounter2",
+		"animNum3",
+	}
+}
 
 function module:Setup(server)
     module.server = server
+
+   local Rig = path.Assets.R15Rig
+   local Animator = Rig.Humanoid.Animator
+	
+   for _, name in pairs(Animations) do
+      if Animator[name] then
+         local animation = Animator[name]
+         Keyframes:InsertAnimations(name, animation) --TODO: Parent the animation's keyframes to the animation (Temporary fix)
+         --Keyframes:InsertAnimations(name, animation:FindFirstChildWhichIsA("KeyframeSequence")
+      end
+   end
 end
 
 function module:WritePlayerPositions(serverTime)
@@ -24,13 +56,13 @@ function module:WritePlayerPositions(serverTime)
             record.position = characterData:GetPosition() --get current visual position
             record.animations = {
                 animCounter0 = characterData.animCounter0,
-			    animNum0 = characterData.animNum0,
-			    animCounter1 = characterData.animCounter1,
-			    animNum1 = characterData.animNum1,
-			    animCounter2 = characterData.animCounter2,
-			    animNum2 = characterData.animNum2,
-			    animCounter3 = characterData.animCounter3,
-			    animNum3 = characterData.animNum3,
+		animNum0 = characterData.animNum0,
+		animCounter1 = characterData.animCounter1,
+		animNum1 = characterData.animNum1,
+		animCounter2 = characterData.animCounter2,
+		animNum2 = characterData.animNum2,
+		animCounter3 = characterData.animCounter3,
+		animNum3 = characterData.animNum3,
             }
             snapshot.players[playerRecord.userId] = record
         end
@@ -110,10 +142,26 @@ function module:PushPlayerPositionsToTime(playerRecord, serverTime, debugText)
             self.temporaryPositions[userId] = oldPos --Store it
 
             local pos = prevPlayerRecord.position:Lerp(nextPlayerRecord.position, frac)
+            
 
             --place it just how it was when the server saw it
             otherPlayerRecord.chickynoid.hitBox.Position = pos
+            otherPlayerRecord.chickynoid.hitBox.Rig:PivotTo(CFrame.new(pos))
 
+            for _, v in pairs(animStates) do
+                for _, state in pairs(v) do
+                    local counter = prevPlayerRecord.animations[state[1]]
+		    local num = prevPlayerRecord.animations[state[2]]
+
+                    local name = Animations:GetAnimation(num)
+                    Keyframes:SetTime(name, counter/255) --Can someone fact check this?
+                    --Is the counter supposed to be time progressing??
+                    --This might cause animations to go slower than usual, refactor this.
+                end
+            end
+
+            Keyframes:ApplyToRig(otherPlayerRecord.chickynoid.hitBox.Rig)
+			
             if debugFlag == true then
                 local event = {}
                 event.t = Enums.EventType.DebugBox
